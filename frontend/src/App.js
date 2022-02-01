f1ac3f5528d5f14273c0cd584de360011815efb5
egg <eggboissaye@gmail.com>
2022-01-31 19:22:49 -0500

    Added frontend files and started homepage

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+import HomePage from "../src/components/pages/HomePage"
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+      </Routes>    
+    </Router>
+  )
+}
+
+export default App;
---
+++ frontend/src/components/pages/HomePage.js
@@ -0,0 +1,13 @@
+import "../../css/all-page-styles.css"
+import "../../css/home-page-styles.css"
+
+const HomePage = () => {
+    return (
+        <>
+            <h1>Earth Recipes</h1>
+            <p>The only recipe site you'll ever need.</p>
+        </>
+    )
+}
+
+export default HomePage
